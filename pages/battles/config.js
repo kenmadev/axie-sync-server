a74632ea13c797a563f0da41fdb4330fd32a531a
Kenma <kenmadev0@gmail.com>
2021-12-01 18:08:26 +0800

    removed unused config file

--- pages/battles/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // REMOTEAPI: 'https://axie-sync-server.vercel.app/api',
-  REMOTEAPI: 'https://axie-sync-server.herokuapp.com/api',
-  REMOTEAPIDEV: 'http://localhost:4000/api',
-};
